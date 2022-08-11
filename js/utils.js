e3199eb5b3a9f5d648ae91ab94d731b8cacf0b4c
Zhicheng2001 <98645864+Zhicheng2001@users.noreply.github.com>
2022-08-11 13:27:01 +0800

    Site updated: 2022-08-11 13:27:01

--- js/utils.js
+++ js/utils.js
@@ -330,25 +330,6 @@ KEEP.initUtils = () => {
 
       }, 200);
     },
-
-    // loading hitokoto
-    initHitokoto() {
-
-      if (KEEP.theme_config.style.first_screen.enable === true && KEEP.theme_config.style.first_screen.hitokoto && window.location.pathname === '/') {
-
-        fetch(KEEP.theme_config.style.first_screen.hitokoto)
-          .then((res) => {
-            return res.json();
-          })
-          .then((data) => {
-            this.firstScreen_dom.querySelector('.description').innerText = data.hitokoto;
-          })
-          .catch((err) => {
-            console.error(err);
-          })
-      }
-
-    }
   }
 
   // init scroll
@@ -378,7 +359,4 @@ KEEP.initUtils = () => {
   // set how long age in home article block
   KEEP.utils.setHowLongAgoInHome();
  
-  // loading hitokoto
-  KEEP.utils.initHitokoto();
-
 }
